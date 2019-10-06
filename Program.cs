ccdfa905c8aab26a056276b6b63ac107da8475b1
Valeria Ivanuik <45975192+ler0nvaler0n@users.noreply.github.com>
2019-10-06 15:59:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2lera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double s = 0;
+            int a1;
+            int a2;
+            int n = 0;
+            bool c;
+            bool b;
+            do
+            {
+                Console.WriteLine("Input first num:");
+                b = int.TryParse(Console.ReadLine(), out a1);
+                Console.WriteLine("Input second num:");
+                c = int.TryParse(Console.ReadLine(), out a2);
+            } while ((a1 < 0 || a2 < a1) || !(c & b));
+
+            for (i = a1; i < a2; i++)
+            {
+                s += (Math.Pow(i, 2) + Math.Pow(-1, i - 1) * 2 * i - 1) / (Math.Pow(i, 2) + 8);
+            }
+            Console.WriteLine("S={0}", s);
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
