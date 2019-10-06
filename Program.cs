1ef89e45936f90c10b4e3102ee3284bf376d2c05
Valeria Ivanuik <45975192+ler0nvaler0n@users.noreply.github.com>
2019-10-06 15:55:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3lera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1, a2, a3;
+            Console.WriteLine("Input first num:");
+            a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second num:");
+            a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input third num:");
+            a3 = Convert.ToInt32(Console.ReadLine());
+
+            if ((a1 == a2) || (a1 == a3) || (a2 == a3))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadKey();
+        }
+    }
+}
