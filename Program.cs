519c252c91c0ece72fe94ac06c3b9b6b0f4449c4
Valeria Ivanuik <45975192+ler0nvaler0n@users.noreply.github.com>
2019-10-06 15:48:11 +0300

    LAB 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab1c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, r, v;
+            double p = Math.PI;
+            Console.WriteLine("Enter height: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            v = (p * h * Math.Pow(r, 2)) / 3;
+            Console.WriteLine("Volume: " + v);
+            Console.ReadKey();
+        }
+    }
+}
